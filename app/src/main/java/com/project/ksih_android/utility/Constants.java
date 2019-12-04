8d5d818ee8068b1a59f98c5264c500bed33f3e57
segunfrancis <francis4dblues@gmail.com>
2019-12-04 12:04:42 +0100

    KSIH-android : add sharedPreference for entire app

---
+++ app/src/main/java/com/project/ksih_android/utility/Constants.java
@@ -0,0 +1,10 @@
+package com.project.ksih_android.utility;
+
+/**
+ * Created by SegunFrancis
+ */
+public class Constants {
+    public static String EMAIL_KEY = "user_email";
+    public static String PASSWORD_KEY = "user_password";
+    public static String SHARED_PREF_KEY = "KSIH-android-app";
+}
