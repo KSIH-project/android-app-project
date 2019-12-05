4b23196ba169ce30732d7124cbdab25d074a0d19
segunfrancis <francis4dblues@gmail.com>
2019-12-05 05:40:46 +0100

    KSIH-android : modify sharedPreference for entire app

--- app/src/main/java/com/project/ksih_android/storage/SharedPreferencesStorage.java
+++ app/src/main/java/com/project/ksih_android/storage/SharedPreferencesStorage.java
@@ -30,11 +30,31 @@ public class SharedPreferencesStorage {
         pref.apply();
     }
 
+    public void setSeenOnBoardingScreen(String key, boolean value) {
+        SharedPreferences.Editor pref = mPreferences.edit();
+        pref.putBoolean(key, value);
+        pref.apply();
+    }
+
+    public void setRememberMe(String key, boolean value) {
+        SharedPreferences.Editor pref = mPreferences.edit();
+        pref.putBoolean(key, value);
+        pref.apply();
+    }
+
     public String getUserName(String key) {
         return mPreferences.getString(key, "");
     }
 
     public String getUserPassword(String key) {
         return mPreferences.getString(key, "");
+    }
+
+    public boolean hasSeenonBoardingScreen(String key) {
+        return  mPreferences.getBoolean(key, false);
+    }
+
+    public boolean isRememberMeChecked(String key) {
+        return mPreferences.getBoolean(key, false);
     }
 }
--- app/src/main/java/com/project/ksih_android/utility/Constants.java
+++ app/src/main/java/com/project/ksih_android/utility/Constants.java
@@ -6,5 +6,7 @@ package com.project.ksih_android.utility;
 public class Constants {
     public static String EMAIL_KEY = "user_email";
     public static String PASSWORD_KEY = "user_password";
+    public static String ON_BOARDING_KEY = "user_seen_onboarding_screen";
+    public static String REMEMBER_ME = "user_checks_remember_me_checkbox";
     public static String SHARED_PREF_KEY = "KSIH-android-app";
 }
