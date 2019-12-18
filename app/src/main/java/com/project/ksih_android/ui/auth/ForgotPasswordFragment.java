914a02c54c6f095a297e68b7b08e0b148ca793b0
segunfrancis <francis4dblues@gmail.com>
2019-12-18 14:41:26 +0100

    KSIH-android : implementation - progressBar

--- app/src/main/java/com/project/ksih_android/ui/auth/ForgotPasswordFragment.java
+++ app/src/main/java/com/project/ksih_android/ui/auth/ForgotPasswordFragment.java
@@ -9,9 +9,11 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.project.ksih_android.R;
 import com.project.ksih_android.databinding.FragmentForgotPasswordBinding;
+import com.victor.loading.rotate.RotateLoading;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,15 +21,18 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 import timber.log.Timber;
 
 /**
  * Created by SegunFrancis
  */
+
 public class ForgotPasswordFragment extends Fragment {
 
     private ForgotPasswordViewModel mViewModel;
     private FirebaseAuth mAuth;
+    private FragmentForgotPasswordBinding mForgotPasswordBinding;
 
     @Nullable
     @Override
@@ -37,21 +42,23 @@ public class ForgotPasswordFragment extends Fragment {
     }
 
     private View setUpBindings(Bundle savedInstanceState, LayoutInflater inflater, ViewGroup container) {
-        FragmentForgotPasswordBinding forgotPasswordBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_forgot_password, container, false);
-        onBackPressed(forgotPasswordBinding.forgotPasswordToolbar);
+        mForgotPasswordBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_forgot_password, container, false);
+        onBackPressed(mForgotPasswordBinding.forgotPasswordToolbar);
         mViewModel = ViewModelProviders.of(this).get(ForgotPasswordViewModel.class);
         if (savedInstanceState == null) {
             mViewModel.init();
         }
-        forgotPasswordBinding.setForgotPassword(mViewModel);
+        mForgotPasswordBinding.setForgotPassword(mViewModel);
         setUpButtonClick();
-        return forgotPasswordBinding.getRoot();
+        return mForgotPasswordBinding.getRoot();
     }
 
     private void setUpButtonClick() {
         mViewModel.getButtonClick().observe(this, new Observer<ForgotPasswordField>() {
             @Override
             public void onChanged(ForgotPasswordField authFields) {
+                startProgressBar(mForgotPasswordBinding.progressBar);
+                hideButton(mForgotPasswordBinding.buttonResetPassword);
                 //TODO: Navigate to Login Fragment
                 resetPassword(authFields.getEmail());
             }
@@ -73,11 +80,36 @@ public class ForgotPasswordFragment extends Fragment {
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()) {
                     Toast.makeText(requireContext(), "Check your email for password reset link", Toast.LENGTH_SHORT).show();
+                    stopProgressBar(mForgotPasswordBinding.progressBar);
+                    showButton(mForgotPasswordBinding.buttonResetPassword);
+                    navigateToLoginFragment(mForgotPasswordBinding.forgotPasswordToolbar);
                 } else {
-                    Toast.makeText(requireContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(requireContext(), task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                    stopProgressBar(mForgotPasswordBinding.progressBar);
+                    showButton(mForgotPasswordBinding.buttonResetPassword);
                     Timber.d(task.getException().getLocalizedMessage());
                 }
             }
         });
+    }
+
+    private void navigateToLoginFragment(View v) {
+        Navigation.findNavController(v).navigate(R.id.loginFragment);
+    }
+
+    private void startProgressBar(RotateLoading loading) {
+        loading.start();
+    }
+
+    private void stopProgressBar(RotateLoading loading) {
+        loading.stop();
+    }
+
+    private void showButton(MaterialButton button) {
+        button.setVisibility(View.VISIBLE);
+    }
+
+    private void hideButton(MaterialButton button) {
+        button.setVisibility(View.INVISIBLE);
     }
 }
