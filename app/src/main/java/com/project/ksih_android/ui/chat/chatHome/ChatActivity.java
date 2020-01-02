a2174274b044b0ca3f816d78a48d76f58d42b10f
Sunday David <sundaydavid989@gmail.com>
2020-01-02 21:55:10 +0100

    Create branch for friendsChat

---
+++ app/src/main/java/com/project/ksih_android/ui/chat/chatHome/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.project.ksih_android.ui.chat.chatHome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.ksih_android.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
