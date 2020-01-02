f5751d8be19b60c947220f190872cb6f2dff4433
Sunday David <sundaydavid989@gmail.com>
2020-01-02 22:59:46 +0100

    Added Request Fragment

--- app/src/main/java/com/project/ksih_android/ui/chat/adapters/TabsPagerAdapter.java
+++ app/src/main/java/com/project/ksih_android/ui/chat/adapters/TabsPagerAdapter.java
@@ -1,4 +1,24 @@
 package com.project.ksih_android.ui.chat.adapters;
 
-public class TabsPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    public TabsPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
