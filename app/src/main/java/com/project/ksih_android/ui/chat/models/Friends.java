97f8949573a954ce5c956955a07451a70acae231
Sunday David <sundaydavid989@gmail.com>
2020-01-03 12:05:18 +0100

    finished friends model

--- app/src/main/java/com/project/ksih_android/ui/chat/models/Friends.java
+++ app/src/main/java/com/project/ksih_android/ui/chat/models/Friends.java
@@ -1,4 +1,41 @@
 package com.project.ksih_android.ui.chat.models;
 
 public class Friends {
+    private String user_name;
+    private String user_thumb_image;
+    private String date;
+
+    public Friends(){
+
+    }
+
+    public Friends(String user_name, String user_thumb_image, String date) {
+        this.user_name = user_name;
+        this.user_thumb_image = user_thumb_image;
+        this.date = date;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_thumb_image() {
+        return user_thumb_image;
+    }
+
+    public void setUser_thumb_image(String user_thumb_image) {
+        this.user_thumb_image = user_thumb_image;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
