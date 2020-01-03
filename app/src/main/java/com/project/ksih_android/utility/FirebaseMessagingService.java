93bc96b74d1a3d8befe72b8489e6d42934a33e70
Sunday David <sundaydavid989@gmail.com>
2020-01-03 13:36:04 +0100

    Added dependencies

--- app/src/main/java/com/project/ksih_android/utility/FirebaseMessagingService.java
+++ app/src/main/java/com/project/ksih_android/utility/FirebaseMessagingService.java
@@ -1,4 +1,7 @@
 package com.project.ksih_android.utility;
 
-public class FirebaseMessagingService {
+import com.google.firebase.inappmessaging.FirebaseInAppMessaging;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
 }
