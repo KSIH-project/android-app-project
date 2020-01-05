1d6985c86ee511ca8e839bfe916482cd1b6683d4
Sunday David <sundaydavid989@gmail.com>
2020-01-05 22:17:16 +0100

    initializing Firebase recycler adapter

--- app/src/main/java/com/project/ksih_android/ui/chat/chatFragment/ChatFragment.java
+++ app/src/main/java/com/project/ksih_android/ui/chat/chatFragment/ChatFragment.java
@@ -6,13 +6,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.project.ksih_android.R;
 
 
@@ -21,6 +24,7 @@ import com.project.ksih_android.R;
  */
 public class ChatFragment extends Fragment {
     private ChatViewModel chatViewModel;
+    String current_user_id;
 
     //initialize variables
     private View view;
@@ -29,6 +33,7 @@ public class ChatFragment extends Fragment {
     //firebase variables
     private DatabaseReference friendsDatabaseReference;
     private DatabaseReference userDatabaseReference;
+    private FirebaseAuth mAuth;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -44,10 +49,28 @@ public class ChatFragment extends Fragment {
 
         });
 
+        //initializing variables for recycler view
+        chat_list = root.findViewById(R.id.chatList);
+        mAuth = FirebaseAuth.getInstance();
+        current_user_id = mAuth.getCurrentUser().getUid();
+        friendsDatabaseReference = FirebaseDatabase.getInstance().getReference().child("friends")
+                .child(current_user_id);
+        userDatabaseReference = FirebaseDatabase.getInstance().getReference().child("users");
+
+        chat_list.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        chat_list.setLayoutManager(linearLayoutManager);
 
         return root;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 }
 
 
