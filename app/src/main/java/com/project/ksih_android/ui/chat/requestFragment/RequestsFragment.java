925a917fafc09e134d99d4e5656d18efe462da35
Sunday David <sundaydavid989@gmail.com>
2020-01-06 01:11:44 +0100

    adding request adapter

--- app/src/main/java/com/project/ksih_android/ui/chat/requestFragment/RequestsFragment.java
+++ app/src/main/java/com/project/ksih_android/ui/chat/requestFragment/RequestsFragment.java
@@ -1,10 +1,14 @@
 package com.project.ksih_android.ui.chat.requestFragment;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,13 +20,26 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.makeramen.roundedimageview.RoundedImageView;
 import com.project.ksih_android.R;
 import com.project.ksih_android.ui.chat.models.Requests;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -81,6 +98,127 @@ public class RequestsFragment extends Fragment {
         FirebaseRecyclerOptions<Requests> recyclerOptions = new FirebaseRecyclerOptions.Builder<Requests>()
                 .setQuery(databaseReference, Requests.class)
                 .build();
+
+        //setup firebase recycler Adapter
+        FirebaseRecyclerAdapter<Requests, RequestsVH> adapter = new FirebaseRecyclerAdapter<Requests, RequestsVH>(recyclerOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull RequestsVH holder, int position, @NonNull Requests model) {
+               final String userID = getRef(position).getKey();
+               //handling accept/cancel button
+                DatabaseReference getTypeReference = getRef(position).child("request_type").getRef();
+                getTypeReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()){
+                            String requestType = dataSnapshot.getValue().toString();
+                            holder.verified_icon.setVisibility(View.GONE);
+
+                            if (requestType.equals("received")){
+                                holder.re_icon.setVisibility(View.VISIBLE);
+                                holder.se_icon.setVisibility(View.GONE);
+                                userDatabaseReference.child(userID).addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        final String userName = dataSnapshot.child("user_name").getValue().toString();
+                                        final String userVerified = dataSnapshot.child("verified").getValue().toString();
+                                        final String userThumbPhoto = dataSnapshot.child("user_thumb_image").getValue().toString();
+                                        final String user_status = dataSnapshot.child("user_status").getValue().toString();
+
+                                        holder.name.setText(userName);
+                                        holder.status.setText(user_status);
+
+                                        // set image conditions
+                                        if (!userThumbPhoto.equals("default_image")){
+                                            Picasso.get()
+                                                    .load(userThumbPhoto)
+                                                    .networkPolicy(NetworkPolicy.OFFLINE)
+                                                    .placeholder(R.drawable.default_profile_image)
+                                                    .into(holder.user_photo, new Callback() {
+                                                        @Override
+                                                        public void onSuccess() {
+
+                                                        }
+
+                                                        @Override
+                                                        public void onError(Exception e) {
+                                                            Picasso.get()
+                                                                    .load(userThumbPhoto)
+                                                                    .placeholder(R.drawable.default_profile_image)
+                                                                    .into(holder.user_photo);
+                                                        }
+                                                    });
+                                        }
+
+                                        if (userVerified.contains("true")){
+                                            holder.verified_icon.setVisibility(View.VISIBLE);
+                                        }
+
+                                        holder.itemView.setOnClickListener(view -> {
+                                            CharSequence options[] = new CharSequence[]{
+                                              "Accept Request", "Cancel Request", userName+"'s profile"
+                                            };
+                                            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                                            builder.setItems(options, new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialogInterface, int which) {
+
+                                                    if (which == 0){
+                                                        Calendar myCalender = Calendar.getInstance();
+                                                        SimpleDateFormat currentDate = new SimpleDateFormat("EEEE, dd MMM, yyyy");
+                                                        final String friendshipDate = currentDate.format(myCalender.getTime());
+
+                                                        friendsDatabaseReference.child(user_UId).child("date").setValue(friendshipDate)
+                                                                .addOnCompleteListener(task -> {
+
+                                                                    //check node and delete after friend request is added
+                                                                    friendReqDatabaseReference.child(user_UId).child(userID).removeValue()
+                                                                            .addOnCompleteListener(task12 -> {
+                                                                                if (task12.isSuccessful()){
+                                                                                    // delete from users friend_request node, receiver >> sender >> values
+                                                                                    friendReqDatabaseReference.child(userID).child(user_UId).removeValue()
+                                                                                            .addOnCompleteListener(task1 -> {
+                                                                                                if (task1.isSuccessful()){
+                                                                                                    //after deleting data
+                                                                                                    Snackbar snackbar = Snackbar
+                                                                                                            .make(view, "You are now friends", Snackbar.LENGTH_LONG);
+                                                                                                    //customizing snackbar
+                                                                                                    View sView = snackbar.getView();
+                                                                                                    sView.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.button_color_disabled));
+                                                                                                    snackbar.setTextColor(Color.WHITE);
+                                                                                                    snackbar.show();
+                                                                                                }
+                                                                                            });
+                                                                                }
+                                                                            });
+                                                                });
+                                                    }
+                                                }
+                                            });
+                                        });
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public RequestsVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+        };
     }
 
     public static class RequestsVH extends RecyclerView.ViewHolder{
