dda09614e5da125047661d34a1913cb6ec9a8a35
segunfrancis <francis4dblues@gmail.com>
2020-01-11 13:51:52 +0100

    KSIH-android : delete startup dialog

--- app/src/main/java/com/project/ksih_android/ui/startup/StartUpDetailsFragment.java
+++ app/src/main/java/com/project/ksih_android/ui/startup/StartUpDetailsFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -28,8 +29,9 @@ import static com.project.ksih_android.utility.Constants.STARTUP_FIREBASE_DATABA
 /**
  * A simple {@link Fragment} subclass.
  * <p>
- * It displays details about a startup
+ * {@link StartUpDetailsFragment} displays details about a startup
  */
+
 public class StartUpDetailsFragment extends Fragment {
 
     private StartUpField mField;
@@ -63,16 +65,34 @@ public class StartUpDetailsFragment extends Fragment {
         twitterET.setText(mField.getTwitterUrl());
 
         // On Click of imageView, display a larger image that supports zooming
-        startupLogo.setOnClickListener(view -> {
-            Bundle bundle = new Bundle();
-            bundle.putString("scale_image", mField.getImageUrl());
-            Navigation.findNavController(view).navigate(R.id.action_startUpDetailsFragment_to_zoomImageFragment, bundle);
-        });
+        initImageClick(startupLogo);
 
         setupToolbar(startUpDetailsToolbar);
         return root;
     }
 
+    /**
+     * A method that takes images to another fragment and enables image zoom
+     * <p>
+     * imageUrl is passed as a bundle parameter with the action that opens the net fragment
+     *
+     * @param startupLogo is the imageView that contains the image
+     */
+    private void initImageClick(ImageView startupLogo) {
+        startupLogo.setOnClickListener(view -> {
+            Bundle bundle = new Bundle();
+            bundle.putString("scale_image", mField.getImageUrl());
+            Navigation.findNavController(view).navigate(R.id.action_startUpDetailsFragment_to_zoomImageFragment, bundle);
+        });
+    }
+
+    /**
+     * A method that initializes the toolbar
+     * <p>
+     * It initializes menu item click listeners
+     *
+     * @param startUpDetailsToolbar represents the toolbar to be initialized
+     */
     private void setupToolbar(MaterialToolbar startUpDetailsToolbar) {
         startUpDetailsToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
         startUpDetailsToolbar.setTitle(mField.getStartupName());
@@ -81,7 +101,7 @@ public class StartUpDetailsFragment extends Fragment {
             if (item.getItemId() == R.id.edit_startup) {
                 editStartupDetails();
             } else if (item.getItemId() == R.id.delete_startup) {
-                deleteStartup(mField.getId());
+                showDialog();
             }
             return true;
         });
@@ -107,6 +127,11 @@ public class StartUpDetailsFragment extends Fragment {
         });
     }
 
+    /**
+     * Deletes an image from the firebase storage
+     *
+     * @param fullUrl is the url of the specific image that is to be deleted
+     */
     private void deleteImage(String fullUrl) {
         StorageReference ref = FirebaseStorage.getInstance().getReferenceFromUrl(fullUrl);
         ref.delete().addOnCompleteListener(requireActivity(), task -> {
@@ -116,5 +141,19 @@ public class StartUpDetailsFragment extends Fragment {
                 Toast.makeText(requireContext(), "Image Delete Error: " + task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    /**
+     * Pops up a dialog to warn the user about deleting a startup entry
+     */
+    private void showDialog() {
+        MaterialAlertDialogBuilder dialog = new MaterialAlertDialogBuilder(requireContext());
+        dialog.setMessage("Are you sure you want to delete this Startup?")
+                .setPositiveButton("YES", (dialogInterface, i) -> {
+                    deleteStartup(mField.getId());
+                    dialogInterface.dismiss();
+                })
+                .setNegativeButton("NO", (dialogInterface, i) -> dialogInterface.dismiss());
+        dialog.create().show();
     }
 }
