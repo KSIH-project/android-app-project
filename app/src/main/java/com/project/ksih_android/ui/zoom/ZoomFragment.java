6c45483fd18aa6c34ca5bcd1fd1d9b288c2d810a
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-01-15 13:11:53 +0100

    KSIH task: Added zomImage fragment

---
+++ app/src/main/java/com/project/ksih_android/ui/zoom/ZoomFragment.java
@@ -0,0 +1,38 @@
+package com.project.ksih_android.ui.zoom;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.project.ksih_android.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ZoomFragment extends Fragment {
+
+    ImageView imageView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_zoom, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imageView = view.findViewById(R.id.image_zoom);
+    }
+
+}
