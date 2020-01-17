64857bc7c9d26f66e390b81c3629c3a9bb047bb1
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-01-17 10:35:21 +0100

    KSIH task: added Zoom Fragment

--- app/src/main/java/com/project/ksih_android/ui/zoom/ZoomFragment.java
+++ app/src/main/java/com/project/ksih_android/ui/zoom/ZoomFragment.java
@@ -2,38 +2,55 @@ package com.project.ksih_android.ui.zoom;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.project.ksih_android.R;
+
+import static com.project.ksih_android.utility.Constants.ZOOM_IMAGE_GENERAL_KEY;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ZoomFragment extends Fragment {
 
-
-    ImageView imageView;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_zoom, container, false);
+        MaterialToolbar toolbar = view.findViewById(R.id.zoom_image_toolbar);
+        ImageView zoomImageView = view.findViewById(R.id.image_zoom);
+        if (getArguments() != null) {
+            loadImage(zoomImageView, getArguments().getString(ZOOM_IMAGE_GENERAL_KEY));
+        }
+        navigateBack(toolbar);
         return view;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        imageView = view.findViewById(R.id.image_zoom_pic);
+    /**
+     * Handles back press action when the back button is clicked
+     *
+     * @param toolbar is the toolbar that holds the back button
+     */
+    private void navigateBack(MaterialToolbar toolbar) {
+        toolbar.setNavigationOnClickListener(v -> Navigation.findNavController(v).navigateUp());
     }
 
+    /**
+     * Loads image using {@link Glide} image loading library
+     *
+     * @param imageView is the view that the image is displayed in
+     * @param imageUrl  is the url for the image that is passed from a bundle in the previous fragment
+     */
+    private void loadImage(ImageView imageView, String imageUrl) {
+        Glide.with(requireActivity()).load(imageUrl).into(imageView);
+    }
 }
-
