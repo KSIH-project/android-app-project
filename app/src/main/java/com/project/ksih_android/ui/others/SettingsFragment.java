3dd0fb724674277c137d096b60369626fe504481
Jesil-O-T <toboroweijesil@gmail.com>
2020-01-24 15:57:10 +0100

    fix some few bugs and added sign out function

--- app/src/main/java/com/project/ksih_android/ui/others/SettingsFragment.java
+++ app/src/main/java/com/project/ksih_android/ui/others/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.project.ksih_android.ui.others;
 
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,7 +12,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.auth.FirebaseAuth;
 import com.project.ksih_android.R;
 
 /**
@@ -18,7 +23,8 @@ import com.project.ksih_android.R;
  */
 public class SettingsFragment extends Fragment {
 
-     TextView mTextView;
+     TextView mTextViewAppinfo, textViewAboutKsih, mTextViewSignOut;
+     private FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,13 +32,39 @@ public class SettingsFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        mTextView =root.findViewById(R.id.app_info);
+        mTextViewAppinfo = root.findViewById(R.id.app_info);
+        mTextViewAppinfo.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_nav_settings_to_appInfo));
 
-        mTextView.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_nav_settings_to_appInfo));
-
-        TextView textViewAboutKsih = root.findViewById(R.id.text_about_ksih);
+        textViewAboutKsih = root.findViewById(R.id.text_about_ksih);
         textViewAboutKsih.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_nav_settings_to_aboutKsihFragment));
 
+        mTextViewSignOut = root.findViewById(R.id.text_signout);
+        mTextViewSignOut.setOnClickListener(v -> {
+            dialogQuestion();
+        });
+
         return root;
+    }
+
+    private void userSignOut(){
+        if (mFirebaseAuth.getCurrentUser() != null){
+            mFirebaseAuth.signOut();
+            Toast.makeText(getActivity(), "Signout Successfully", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getActivity(), "Please Sign in!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void dialogQuestion(){
+        MaterialAlertDialogBuilder materialAlertDialogBuilder = new MaterialAlertDialogBuilder(getActivity());
+        materialAlertDialogBuilder.setTitle("Sign out!");
+        materialAlertDialogBuilder.setIcon(R.drawable.ksih_background);
+        materialAlertDialogBuilder.setMessage("Are you sure you want to Sign out?");
+
+        materialAlertDialogBuilder.setPositiveButton("Yes", (dialog, which) -> {userSignOut();});
+        materialAlertDialogBuilder.setNegativeButton("No", (dialog, which) -> {});
+
+        materialAlertDialogBuilder.create().show();
     }
 }
