efb9662d353564408222c1f23e66627fefdf3dfe
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-01-30 22:20:44 +0100

    KSIH ONBOARDING TASK: onboarding ui

---
+++ app/src/main/java/com/project/ksih_android/ui/onBoarding/PagerAdapter.java
@@ -0,0 +1,25 @@
+package com.project.ksih_android.ui.onBoarding;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    public PagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
