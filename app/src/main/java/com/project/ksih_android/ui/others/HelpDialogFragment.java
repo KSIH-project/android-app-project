7bccf0df738e6820c8c537767550b5ee61722099
sundaydavid <sundaydavid989@gmail.com>
2020-02-10 13:58:56 +0100

    finished help

--- app/src/main/java/com/project/ksih_android/ui/others/HelpDialogFragment.java
+++ app/src/main/java/com/project/ksih_android/ui/others/HelpDialogFragment.java
@@ -1,20 +1,30 @@
 package com.project.ksih_android.ui.others;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.project.ksih_android.R;
+
+import static com.project.ksih_android.utility.Constants.ZOOM_IMAGE_GENERAL_KEY;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HelpDialogFragment extends Fragment {
+public class HelpDialogFragment extends DialogFragment {
 
 
     public HelpDialogFragment() {
@@ -22,11 +32,16 @@ public class HelpDialogFragment extends Fragment {
     }
 
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help_dialog, container, false);
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_help_dialog, null);
+        builder.setView(view);
+
+
+        return builder.create();
     }
 
 }
